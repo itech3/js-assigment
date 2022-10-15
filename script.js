7679b0fc48e178914d59f987719008d471d617fd
itech3 <99327485+itech3@users.noreply.github.com>
2022-10-15 15:36:52 +0200

    upload files

---
+++ script.js
@@ -0,0 +1,51 @@
+function computerSelection() {
+    const hand = ['Rock', 'Paper', 'Scissors'];
+    return hand[Math.floor(Math.random() * hand.length )];
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    console.log(`Computer Selection: ${computerSelection}`);
+    
+    if (playerSelection == computerSelection) {
+        return "Draw!!\n";
+    }
+    else if ( (playerSelection == "rock" && computerSelection == "scissors") ) {
+        playerPoints++;
+        return `You Won! ${playerSelection} beats ${computerSelection}\n`;
+    }
+    
+    else if ( (playerSelection == "scissors" && computerSelection == "paper") ) {
+        playerPoints++;
+        return `You Won! ${playerSelection} beats ${computerSelection}\n`;
+    }
+
+    else if ( (playerSelection == "paper" && computerSelection == "rock") ) {
+        playerPoints++;
+        return `You Won! ${playerSelection} beats ${computerSelection}\n`;
+    }
+
+    else {
+        computerPoints++;
+        return `You Lose! ${computerSelection} beats ${playerSelection}\n`
+    }
+}
+
+function game() {
+
+    for (let turn = 0; turn < 5; turn++) {
+        let userSelection = prompt("Please Select: Rock, Paper, Scissors")
+        console.log( `Winner: ${ playRound( userSelection, computerSelection() ) }` );
+    }
+
+    console.log(`\n\nPlayer Points: ${playerPoints}`);
+    console.log(`Computer Points: ${computerPoints}`);
+
+}
+
+let playerPoints = 0;
+let computerPoints = 0;
+
+game();
